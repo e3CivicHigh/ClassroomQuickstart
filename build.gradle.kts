f1d5d2cc747feb127ef8c4757ce13ccd7c5f0843
Stephen Cerruti <steve.cerruti@gmail.com>
2024-02-02 09:49:05 -0800

    Initial version

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.e3civichigh"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.api-client:google-api-client:2.0.0")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.apis:google-api-services-classroom:v1-rev20220323-2.0.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
